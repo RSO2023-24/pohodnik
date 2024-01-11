e993a95a6b9e1d066824954f6d338d9fd4268108
Generalsava <zan24pockar@gmail.com>
2024-01-11 14:09:42 +0100

    Final

--- models/target/classes/sql-scripts/init.sql
+++ models/target/classes/sql-scripts/init.sql
@@ -1,4 +1,4 @@
-INSERT INTO image_metadata (first_name, last_name, age, sex, skill, longtitude, latitude) VALUES ('Tom', 'Swayer', 98, "male", 7, 47.0516, 15.503);
+INSERT INTO image_metadata (first_name, last_name, age, sex, skill, longtitude, latitude) VALUES ('Tom', 'Swayer', 98, 'male', 7, 47.0516, 15.503);
 INSERT INTO image_metadata (first_name, last_name, age, sex, skill, longtitude, latitude) VALUES ('Andrej', 'Hrovat', 34, 'male', 7, 46.043339, 14.504097);
 INSERT INTO image_metadata (first_name, last_name, age, sex, skill, longtitude, latitude) VALUES ('Luka', 'Novak', 45, 'male', 8, 46.048353, 14.499680);
 INSERT INTO image_metadata (first_name, last_name, age, sex, skill, longtitude, latitude) VALUES ('Peter', 'Mlakar', 35, 'male', 5, 46.050599, 14.503780);
